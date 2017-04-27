1d9f9550a3edf46a27e0fab4c46e1cb688a44941
chenchc <bryan830401@gmail.com>
2017-04-27 23:00:13 +0800

    Fix some bugs.

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/wp57qiorhl0xoy3/s2vt_final.tar?dl=0 -O s2vt_final.tar; tar xf s2vt_final.tar
+wget http://speech.ee.ntu.edu.tw/~yangchiyi/MLDS_hw2/MLDS_hw2_data.tar.gz -O MLDS_hw2_data.tar; tar xf MLDS_hw2_data.tar
+wget https://www.dropbox.com/s/kc28n3zpu228368/s2vt_final.tar?dl=0 -O s2vt_final.tar; tar xf s2vt_final.tar
 rm -rf test
 mkdir test
 cp $1 test/testing_id.txt
