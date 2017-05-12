cafbff3692a442ab24354ddc174e2579d57be7bc
chenchc <bryan830401@gmail.com>
2017-05-13 03:58:30 +0800

    BEGAN.

---
+++ hw3/src/model_began.py
@@ -0,0 +1,357 @@
+import numpy as np
+import os
+import sys
+import random
+import tensorflow as tf
+import time
+import utils
+
+class ModelBEGAN:
+	
+	## Config
+	batch_size = 16
+	filter_dim = 128
+	latent_dim = 64
+	gamma = 0.3
+	lambda_k = 0.001
+	lr_init = 0.0001
+	lr_decay_steps = 100
+	lr_decay_rate = 1.0
+	num_sample = 5
+
+	model_name = 'began'
+	save_dir = 'data/model_' + model_name
+	save_path = save_dir + '/model.ckpt'
+
+	## Constants
+	data = None
+	num_color = None
+
+	## Placeholder tensors
+	real_image = None
+	hair_color = None
+	eyes_color = None
+
+	## Output tensors
+	update_k = None
+	convergence_measure = None
+	train_step = None
+	gen_image_uint8 = None
+	loss_gen_image_batch = None
+
+	def __init__(self):
+		if not os.path.exists(self.save_dir):
+			os.makedirs(self.save_dir)
+
+		self.__fillConstants()
+		self.__defineModel()
+
+	def __fillConstants(self):
+		self.data = utils.Utils()
+		self.num_color = len(self.data.color_list)
+
+	def __defineModel(self):
+		print 'Defining model...'
+
+		## Global step
+		global_step = tf.get_variable('global_step', shape=[], dtype=tf.int32, initializer=tf.constant_initializer(0), trainable=False)
+
+		## Input placeholders
+		self.real_image = real_image = tf.placeholder(tf.uint8, shape=[self.batch_size]+self.data.TRAIN_IMAGE_SHAPE, name='real_image')
+		real_image = tf.image.convert_image_dtype(real_image, tf.float32)
+		self.hair_color = hair_color = tf.placeholder(tf.int32, shape=[self.batch_size], name='hair_color')
+		self.eyes_color = eyes_color = tf.placeholder(tf.int32, shape=[self.batch_size], name='eyes_color')
+		
+		## Generator
+		z = tf.random_uniform([self.batch_size, self.latent_dim], minval=-1.0, maxval=1.0, dtype=tf.float32, name='z')
+		gen_image = self.__defineGenerator(z)
+		self.gen_image_uint8 = gen_image_uint8 = tf.image.convert_image_dtype(gen_image, tf.uint8, name='gen_image_uint8')
+		
+		## Discriminator
+		restored_real_image = self.__defineDiscriminator(real_image)
+		restored_gen_image = self.__defineDiscriminator(gen_image, reuse_scope=True)
+		
+		## Define reconstruction loss
+		def reconLoss(img, restored_img, scope, reserve_batch=False):
+			with tf.variable_scope(scope):
+				recon_loss = tf.abs(img - restored_img)
+				if reserve_batch:
+					recon_loss = tf.reshape(recon_loss, [self.batch_size, -1])
+					recon_loss = tf.reduce_mean(recon_loss, axis=1, name='recon_loss')
+				else:
+					recon_loss = tf.reduce_mean(recon_loss, name='recon_loss')
+			return recon_loss
+		
+		## Loss
+		k = tf.get_variable('k', shape=[], dtype=tf.float32, initializer=tf.constant_initializer(0.0), trainable=False)
+
+		loss_real_image = reconLoss(real_image, restored_real_image, 'real_image')
+		loss_gen_image = reconLoss(gen_image, restored_gen_image, 'gen_image')
+		self.loss_gen_image_batch = reconLoss(gen_image, restored_gen_image, 'gen_image_batch', reserve_batch=True)
+		loss_generator = loss_gen_image
+		loss_discriminator = loss_real_image - k * loss_gen_image
+		
+		self.update_k = update_k = tf.assign_add(k, self.lambda_k * (self.gamma * loss_real_image - loss_gen_image), name='update_k')
+		self.convergence_measure = convergence_measure = tf.add(loss_real_image, tf.abs(self.gamma * loss_real_image - loss_gen_image), name='convergence_measure')
+		
+		## Optimization
+		lr = tf.train.exponential_decay(self.lr_init, global_step, self.lr_decay_steps, self.lr_decay_rate)
+		optimizer = tf.train.AdamOptimizer(lr)
+		grad_loss_generator = optimizer.compute_gradients(loss_generator, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'generator/'))
+		grad_loss_discriminator = optimizer.compute_gradients(loss_discriminator, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator/'))
+		self.train_step = train_step = optimizer.apply_gradients(grad_loss_generator + grad_loss_discriminator, global_step=global_step, name='train_step')
+		
+		## Print trainable variables
+		print 'Variables:'
+		for i in tf.global_variables():
+			print i.name, i.get_shape()
+
+	def __defineGenerator(self, z, reuse_scope=False):
+		with tf.variable_scope('generator') as scope:
+			if reuse_scope:
+				scope.reuse_variables()
+
+			def doConv(input, filters):
+				output = tf.layers.conv2d(input, filters, [3, 3], padding='SAME')
+				output = tf.nn.elu(output, name='output')
+				return output
+
+			def doUpscaling(input, size):
+				output = tf.image.resize_nearest_neighbor(input, size, name='output')
+				return output
+
+			with tf.variable_scope('layer1'):
+				w = tf.get_variable('w', shape=[self.latent_dim, 8 * 8 * self.filter_dim], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+				b = tf.get_variable('b', shape=[8 * 8 * self.filter_dim], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+				layer1 = tf.reshape(tf.add(tf.matmul(z, w), b), [self.batch_size, 8, 8, self.filter_dim], name='output')
+
+			with tf.variable_scope('conv1_1'):
+				conv1_1 = doConv(layer1, self.filter_dim)
+
+			with tf.variable_scope('conv1_2'):
+				conv1_2 = doConv(conv1_1, self.filter_dim)
+
+			with tf.variable_scope('layer2'):
+				layer2 = doUpscaling(conv1_2, [16, 16])
+
+			with tf.variable_scope('conv2_1'):
+				conv2_1 = doConv(layer2, self.filter_dim)
+
+			with tf.variable_scope('conv2_2'):
+				conv2_2 = doConv(conv2_1, self.filter_dim)
+
+			with tf.variable_scope('layer3'):
+				layer3 = doUpscaling(conv2_2, [32, 32])
+
+			with tf.variable_scope('conv3_1'):
+				conv3_1 = doConv(layer3, self.filter_dim)
+
+			with tf.variable_scope('conv3_2'):
+				conv3_2 = doConv(conv3_1, self.filter_dim)
+
+			with tf.variable_scope('layer4'):
+				layer4 = doUpscaling(conv3_2, [64, 64])
+
+			with tf.variable_scope('conv4_1'):
+				conv4_1 = doConv(layer4, self.filter_dim)
+
+			with tf.variable_scope('conv4_2'):
+				conv4_2 = doConv(conv4_1, self.filter_dim)
+
+			with tf.variable_scope('output'):
+				output = tf.layers.conv2d(conv4_2, 3, [3, 3], padding='SAME')
+				output = tf.nn.sigmoid(output, name='output')
+
+		return output
+				
+	def __defineDiscriminator(self, image, reuse_scope=False):
+		
+		with tf.variable_scope('discriminator') as scope:
+			if reuse_scope:
+				scope.reuse_variables()
+
+			def doConv(input, filters):
+				output = tf.layers.conv2d(input, filters, [3, 3], padding='SAME')
+				output = tf.nn.elu(output, name='output')
+				return output
+
+			def doUpscaling(input, size):
+				output = tf.image.resize_nearest_neighbor(input, size, name='output')
+				return output
+
+			def doDownscaling(input, size):
+				output = tf.layers.average_pooling2d(input, [2, 2], [2, 2], name='output')
+				assert size == output.shape.as_list()[1:3]
+				return output
+			
+			with tf.variable_scope('encoder'):
+				with tf.variable_scope('conv1_1'):
+					conv1_1 = doConv(image, self.filter_dim)
+
+				with tf.variable_scope('conv1_2'):
+					conv1_2 = doConv(conv1_1, self.filter_dim)
+
+				with tf.variable_scope('layer2'):
+					layer2 = doDownscaling(conv1_2, [32, 32])
+
+				with tf.variable_scope('conv2_1'):
+					conv2_1 = doConv(layer2, self.filter_dim * 2)
+
+				with tf.variable_scope('conv2_2'):
+					conv2_2 = doConv(conv2_1, self.filter_dim * 2)
+
+				with tf.variable_scope('layer3'):
+					layer3 = doDownscaling(conv2_2, [16, 16])
+
+				with tf.variable_scope('conv3_1'):
+					conv3_1 = doConv(layer3, self.filter_dim * 3)
+
+				with tf.variable_scope('conv3_2'):
+					conv3_2 = doConv(conv3_1, self.filter_dim * 3)
+
+				with tf.variable_scope('layer4'):
+					layer4 = doDownscaling(conv3_2, [8, 8])
+
+				with tf.variable_scope('conv4_1'):
+					conv4_1 = doConv(layer4, self.filter_dim * 4)
+
+				with tf.variable_scope('conv4_2'):
+					conv4_2 = doConv(conv4_1, self.filter_dim * 4)
+
+				with tf.variable_scope('z'):
+					conv4_2 = tf.reshape(conv4_2, [self.batch_size, 8 * 8 * self.filter_dim * 4])
+					w = tf.get_variable('w', shape=[8 * 8 * self.filter_dim * 4, self.latent_dim], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+					b = tf.get_variable('b', shape=[self.latent_dim], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+					z = tf.add(tf.matmul(conv4_2, w), b, name='z')
+
+			with tf.variable_scope('decoder'):
+				with tf.variable_scope('layer1'):
+					w = tf.get_variable('w', shape=[self.latent_dim, 8 * 8 * self.filter_dim], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+					b = tf.get_variable('b', shape=[8 * 8 * self.filter_dim], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+					dec_layer1 = tf.reshape(tf.add(tf.matmul(z, w), b), [self.batch_size, 8, 8, self.filter_dim], name='output')
+
+				with tf.variable_scope('conv1_1'):
+					dec_conv1_1 = doConv(dec_layer1, self.filter_dim)
+
+				with tf.variable_scope('conv1_2'):
+					dec_conv1_2 = doConv(dec_conv1_1, self.filter_dim)
+
+				with tf.variable_scope('layer2'):
+					dec_layer2 = doUpscaling(dec_conv1_2, [16, 16])
+
+				with tf.variable_scope('conv2_1'):
+					dec_conv2_1 = doConv(dec_layer2, self.filter_dim)
+
+				with tf.variable_scope('conv2_2'):
+					dec_conv2_2 = doConv(dec_conv2_1, self.filter_dim)
+
+				with tf.variable_scope('layer3'):
+					dec_layer3 = doUpscaling(dec_conv2_2, [32, 32])
+
+				with tf.variable_scope('conv3_1'):
+					dec_conv3_1 = doConv(dec_layer3, self.filter_dim)
+
+				with tf.variable_scope('conv3_2'):
+					dec_conv3_2 = doConv(dec_conv3_1, self.filter_dim)
+
+				with tf.variable_scope('layer4'):
+					dec_layer4 = doUpscaling(dec_conv3_2, [64, 64])
+
+				with tf.variable_scope('conv4_1'):
+					dec_conv4_1 = doConv(dec_layer4, self.filter_dim)
+
+				with tf.variable_scope('conv4_2'):
+					dec_conv4_2 = doConv(dec_conv4_1, self.filter_dim)
+
+				with tf.variable_scope('output'):
+					output = tf.layers.conv2d(dec_conv4_2, 3, [3, 3], padding='SAME')
+					output = tf.nn.sigmoid(output, name='output')
+			
+		return output
+
+	def __prepareTrainPairList(self):
+		train_pair_list = []
+		for idx in range(len(self.data.train_image_list)):
+			train_pair_list.append((self.data.train_image_list[idx], self.data.train_hair_color_list[idx], self.data.train_eyes_color_list[idx]))
+		random.shuffle(train_pair_list)
+
+		return train_pair_list
+
+	def __prepareTrainFeedDictList(self, pair_list, index):
+		real_image_data = np.zeros(shape=[self.batch_size]+self.data.TRAIN_IMAGE_SHAPE, dtype=np.uint8)
+		hair_color_data = np.zeros(shape=(self.batch_size), dtype=np.int32)
+		eyes_color_data = np.zeros(shape=(self.batch_size), dtype=np.int32)
+
+		for i in range(self.batch_size):
+			real_image_data[i] = pair_list[index + i][0]
+			hair_color_data[i] = self.data.color_index_dict[pair_list[index + i][1]]
+			eyes_color_data[i] = self.data.color_index_dict[pair_list[index + i][2]]
+
+		feed_dict = {
+			self.real_image: real_image_data,
+			self.hair_color: hair_color_data,
+			self.eyes_color: eyes_color_data
+			}
+
+		return feed_dict
+
+	def __prepareTestFeedDictList(self, testing_id):
+		hair_color_data = np.zeros(shape=(self.batch_size), dtype=np.int32)
+		eyes_color_data = np.zeros(shape=(self.batch_size), dtype=np.int32)
+
+		for i in range(self.batch_size):
+			hair_color_data[i] = self.data.color_index_dict[self.data.test_list[testing_id]['hair']]
+			eyes_color_data[i] = self.data.color_index_dict[self.data.test_list[testing_id]['eyes']]
+
+		feed_dict = {
+			self.hair_color: hair_color_data,
+			self.eyes_color: eyes_color_data
+			}
+
+		return feed_dict
+
+	def __testFast(self, sess):
+		feed_dict = {
+			self.hair_color: np.zeros(shape=(self.batch_size), dtype=np.int32),
+			self.eyes_color: np.zeros(shape=(self.batch_size), dtype=np.int32)
+			}
+		gen_image, loss = sess.run([self.gen_image_uint8, self.loss_gen_image_batch], feed_dict=feed_dict)
+		index = np.argmin(loss)
+		self.data.saveImage('training', '0', gen_image[index])
+
+	def train(self, savepoint=None):
+		print 'Start training...'
+
+		config = tf.ConfigProto()
+		config.gpu_options.allow_growth = True
+		with tf.Session(config=config) as sess:
+			init = tf.global_variables_initializer()
+			sess.run(init)
+			if savepoint != None:
+				tf.train.Saver().restore(sess, savepoint)
+
+			for epoch in range(0, 1000, 1):
+				print '[Epoch #{}]'.format(str(epoch))
+
+				train_pair_list = self.__prepareTrainPairList()
+				
+				convergence_measure_history = []
+				percent = 1
+				for step in range(0, len(train_pair_list) - self.batch_size, self.batch_size):
+					feed_dict = self.__prepareTrainFeedDictList(train_pair_list, step)
+					_, _, convergence_measure = sess.run([self.train_step, self.update_k, self.convergence_measure], feed_dict=feed_dict)
+					convergence_measure_history.append(convergence_measure)
+					if step > percent * len(train_pair_list) / 100:
+						print 'Progress: {}%, Convergence measure: {}'.format(percent, np.mean(convergence_measure_history))
+						convergence_measure_history = []
+						self.__testFast(sess)
+						
+						percent += 1
+
+				tf.train.Saver().save(sess, self.save_path)
+			
+		
+if __name__ == '__main__':
+	model = ModelBEGAN()
+	model.train('data/model_began/model.ckpt')
+	#model.train()
